da3b686b27d4464e641255120aaf998db9d0bcd9
Apoorv0503 <apdwi.ad@gmail.com>
2023-02-11 23:05:24 +0530

    initial commit

--- app.js
+++ app.js
@@ -286,9 +286,13 @@ else {
 });
 
 
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+const port= process.env.PORT;
+if(port== null || port=="")
+{
+  port=3000;
+}
+app.listen(port, function () {
+  console.log("Server running succesfully");
 });
 
 
